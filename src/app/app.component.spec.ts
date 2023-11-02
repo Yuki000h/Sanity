19c433a173c7549f9a57d55c42a3d14da3cfa08a
Yuki000h <thediamondpegamer@gmail.com>
2023-11-02 13:45:19 +0800

    asdasdadda

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
+
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,11 +16,13 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+
   it(`should have as title 'crud-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('crud-app');
   });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
---
+++ src/app/core/core.service.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed } from '@angular/core/testing';
+
+
+import { CoreService } from './core.service';
+
+
+describe('CoreService', () => {
+  let service: CoreService;
+
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CoreService);
+  });
+
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
